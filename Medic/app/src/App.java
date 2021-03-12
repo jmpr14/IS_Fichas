e34c476911a24e1ff5da1e8db31c1b6f8d0ff750
EduardoCosta99 <50023394+EduardoCosta99@users.noreply.github.com>
2021-03-12 16:42:33 +0000

    start medic

---
+++ Medic/app/src/App.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        String pass = "";
+        try {
+            FileReader arq = new FileReader("configs.txt");
+            BufferedReader lerArq = new BufferedReader(arq);
+            pass = lerArq.readLine();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        DataBase bd = new DataBase(pass);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int op = -1;
+
+        while (op != 0) {
+
+            System.out.println("\n> INDIQUE O NÚMERO DA OPERAÇÃO");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Novo Pedido");
+            System.out.println("2 - Ver Pedidos");
+            System.out.println("3 - Cancelar Pedido");
+
+            try {
+                op = Integer.parseInt(reader.readLine());
+
+                switch (op) {
+                    case 0:
+                        System.out.println("\nA SAIR");
+                        break;
+                    case 1:
+                        System.out.println("-- Insira o número de Utente do paciente");
+                        String numUtente = reader.readLine();
+                        int id_doente;
+                        if (!bd.checkDoente(numUtente)) {
+                            System.out.println("---- NOVO REGISTO DE DOENTE");
+                            System.out.println("-- Insira o nome do paciente");
+                            String nome = reader.readLine();
+                            System.out.println("-- Insira a morada do paciente");
+                            String morada = reader.readLine();
+                            System.out.println("-- Insira o telefone do paciente");
+                            String telefone = reader.readLine();
+
+                            id_doente = bd.insertDoente(nome, telefone, numUtente, morada);
+                        } else id_doente = bd.getIdDoente(numUtente);
+                        System.out.println("-- Insira a sigla do tipo de Exame");
+                        String siglaExame = reader.readLine();
+                        System.out.println("-- Insira uma descrição acerca do exame (opcional)");
+                        String descricao = reader.readLine();
+
+                        int id_exame = bd.insertExame(descricao, siglaExame);
+                        bd.insertPedido(id_exame, id_doente);
+
+                        break;
+                    case 2:
+                        bd.showPedidos();
+                        break;
+                    case 3:
+                        System.out.println("-- Insira o número do Pedido a cancelar");
+                        String id_Pedido = reader.readLine();
+                        if (bd.cancelarPedido(id_Pedido)) System.out.println("Pedido Cancelado com sucesso!");
+                        else System.out.println("Operação inválida!");
+                        break;
+                    default:
+                        System.out.println("\nInsira uma operação válida!");
+                        break;
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+    }
+}
---
+++ Medic/app/src/DataBase.java
@@ -0,0 +1,219 @@
+import java.sql.*;
+
+public class DataBase {
+
+    private final String pass;
+
+    public DataBase(String pass) {
+        this.pass = pass;
+    }
+
+    public boolean checkDoente(String numUtente) {
+        boolean a = true;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/desk_services?user=root&password="
+                            + this.pass + "&useTimezone=true&serverTimezone=UTC");
+
+            Statement stmt = conn.createStatement();
+
+            String sql = "select * from Doente where num_utente=" + numUtente + ";";
+            ResultSet rss = stmt.executeQuery(sql);
+
+            if (!rss.next()) a = false;
+            conn.close();
+
+            return a;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return a;
+    }
+
+    public int insertExame(String descricao, String sigla) {
+        int a = 0;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/desk_services?user=root&password="
+                            + this.pass + "&useTimezone=true&serverTimezone=UTC");
+
+            Statement stmt = conn.createStatement();
+
+            stmt.execute("insert into Exame values(0,\"" + descricao + "\", \"" + "" + "\", \"" + sigla + "\");");
+
+            Statement select = conn.createStatement();
+
+            String sql = "SELECT LAST_INSERT_ID();";
+            ResultSet rss = select.executeQuery(sql);
+            rss.next();
+            a = rss.getInt(1);
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return a;
+    }
+
+    public int insertDoente(String nome, String telefone, String num_utente, String morada) {
+        int a = 0;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/desk_services?user=root&password="
+                            + this.pass + "&useTimezone=true&serverTimezone=UTC");
+
+            Statement stmt = conn.createStatement();
+
+            stmt.execute("insert into Doente (num_utente,nome,telefone,morada) values(\"" + num_utente + "\", \"" + nome + "\", \"" + telefone + "\", \"" + morada + "\");");
+
+            Statement select = conn.createStatement();
+
+            String sql = "select id_doente from Doente where num_utente=" + num_utente + ";";
+            ResultSet rss = select.executeQuery(sql);
+            rss.next();
+            a = rss.getInt(1);
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return a;
+    }
+
+    public int getIdDoente(String num_utente) {
+        int a = 0;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/desk_services?user=root&password="
+                            + this.pass + "&useTimezone=true&serverTimezone=UTC");
+
+            Statement select = conn.createStatement();
+
+            String sql = "select id_doente from Doente where num_utente=" + num_utente + ";";
+            ResultSet rss = select.executeQuery(sql);
+            rss.next();
+            a = rss.getInt(1);
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return a;
+    }
+
+    public void insertPedido(int id_exame, int id_doente) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/desk_services?user=root&password="
+                            + this.pass + "&useTimezone=true&serverTimezone=UTC");
+
+            Statement stmt = conn.createStatement();
+
+            stmt.execute("insert into Pedido values(0, NOW(), \"" + "0" + "\", \"" + "0" + "\", \"" + id_exame + "\", \"" + id_doente + "\");");
+
+            conn.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void showPedidos() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/desk_services?user=root&password="
+                            + this.pass + "&useTimezone=true&serverTimezone=UTC");
+
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery("select * from Pedido");
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            System.out.println("--------------------------------------------------------------------------");
+            System.out.println("| PEDIDO |      DATETIME      |EPISÓDIO|   ESTADO   |  EXAME  | PACIENTE");
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i == 1) System.out.print("    ");
+                    else System.out.print("     ");
+                    String columnValue = rs.getString(i);
+                    switch (i) {
+                        case 4:
+                            if (Integer.parseInt(columnValue) == 0) System.out.print("Em espera");
+                            else if (Integer.parseInt(columnValue) == 1) System.out.print("Realizado");
+                            else System.out.print("Cancelado");
+                            break;
+                        case 5:
+                            int id_Exame = Integer.parseInt(columnValue);
+                            Statement media1 = conn.createStatement();
+                            ResultSet rss1 = media1.executeQuery("select sigla from Exame where id_exame=" + id_Exame + ";");
+                            rss1.next();
+                            System.out.print(rss1.getString(1));
+                            break;
+                        case 6:
+                            int id_Doente = Integer.parseInt(columnValue);
+                            Statement media2 = conn.createStatement();
+                            ResultSet rss2 = media2.executeQuery("select nome from Doente where id_doente=" + id_Doente + ";");
+                            rss2.next();
+                            System.out.print(rss2.getString(1));
+                            break;
+                        default:
+                            System.out.print(columnValue);
+                            break;
+                    }
+
+                }
+                System.out.println();
+            }
+            System.out.println("--------------------------------------------------------------------------\n");
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public boolean cancelarPedido(String id_Pedido) {
+        boolean a = true;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.
+                    getConnection("jdbc:mysql://localhost:3306/desk_services?user=root&password="
+                            + this.pass + "&useTimezone=true&serverTimezone=UTC");
+
+            Statement select = conn.createStatement();
+
+            String sql = "select estado from Pedido WHERE id_pedido = " + id_Pedido + ";";
+            ResultSet rss = select.executeQuery(sql);
+            rss.next();
+
+            if (rss.getInt(1) == 0) {
+                Statement stmt = conn.createStatement();
+                stmt.execute("UPDATE Pedido SET estado = 2 WHERE id_pedido = " + id_Pedido + ";");
+            } else a = false;
+
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return a;
+    }
+
+
+}
