b598ecd70b359db5a9744e07b7a241abef802080
EduardoCosta99 <50023394+EduardoCosta99@users.noreply.github.com>
2021-03-25 23:09:14 +0000

    another one

--- Medic/database/testing.sql
+++ Medic/database/testing.sql
@@ -3,16 +3,16 @@ delete FROM exame;
 delete FROM doente;
 
 ALTER TABLE pedido AUTO_INCREMENT = 1;
+ALTER TABLE exame AUTO_INCREMENT = 1;
+ALTER TABLE doente AUTO_INCREMENT = 1;
 
-INSERT INTO doente VALUES(DEFAULT,"123456789","Antonio Silva","927777777","Rua da Universidade, n10");
-INSERT INTO doente VALUES(DEFAULT,"12345","Maria Ferreira","914444444","Rua dos Chaos, n8 - Braga");
+INSERT INTO doente VALUES(DEFAULT,"123456789","Jose Ferreira","911111111","Rua da Universidade");
+INSERT INTO doente VALUES(DEFAULT,"12345","Antonio Silva","999999999","Rua dos Chaos, n8");
 
-insert into exame values (DEFAULT,"","TAC");
-insert into exame values (DEFAULT,"","BIOPSIA");
 insert into exame values (DEFAULT,"","CARDIO");
-insert into exame values (DEFAULT,"","CARDIO");
+insert into exame values (DEFAULT,"","RADIO");
+insert into exame values (DEFAULT,"","QUIMIO");
 
-insert into pedido values (DEFAULT,NOW(),0,0,1,1,"TAC urgente.");
-insert into pedido values (DEFAULT,NOW(),0,0,2,1,"Biopsia nodulo estomago.");
-insert into pedido values (DEFAULT,NOW(),0,0,3,2,"Ecocardiograma urgente.");
-insert into pedido values (DEFAULT,NOW(),0,0,4,2,"Prova de esforço de rotina.");
+insert into pedido values (DEFAULT,NOW(),0,0,1,1,"ecocardiograma e prova de esforço");
+insert into pedido values (DEFAULT,NOW(),0,0,2,1,"radiografia urgente!");
+insert into pedido values (DEFAULT,NOW(),0,2,3,2,"");
--- Services/database/testing.sql
+++ Services/database/testing.sql
@@ -1,7 +1,12 @@
+delete FROM desk_services.worklist;
 delete FROM desk_services.pedido;
 delete FROM desk_services.exame;
+delete FROM desk_services.doente;
 
-ALTER TABLE doente AUTO_INCREMENT = 1;
+ALTER TABLE desk_services.worklist AUTO_INCREMENT = 1;
+ALTER TABLE desk_services.pedido AUTO_INCREMENT = 1;
+ALTER TABLE desk_services.exame AUTO_INCREMENT = 1;
+ALTER TABLE desk_services.doente AUTO_INCREMENT = 1;
 
 
 insert into exame values (DEFAULT,"O paciente apresentou vários nodos suspeitos que precisam de análise.","TAC");
