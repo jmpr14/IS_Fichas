af70f2cd42e3f74fbe1bbc47f974a08c5bd78aad
jmpr14 <49024191+jmpr14@users.noreply.github.com>
2021-04-30 14:08:41 +0100

    cria pasta

--- Ficha02/Medic/RESTServer/src/main/java/aebd/group/webapp/EscreveFicheiro.java
+++ Ficha02/Medic/RESTServer/src/main/java/aebd/group/webapp/EscreveFicheiro.java
@@ -1,13 +1,18 @@
 package aebd.group.webapp;
 
+import javax.validation.Path;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.nio.file.Files;
 
 public class EscreveFicheiro {
 
     public static void escrever(String json, String id_pedido){
         try {
+            File dir = new File("..\\Received");
+            if(!dir.exists())
+                Files.createDirectory(dir.toPath());
             File fich = new File("..\\Received\\" + id_pedido + ".json");
             BufferedWriter buffWrite = new BufferedWriter(new FileWriter(fich.getPath()));
             buffWrite.write(json);
--- Ficha02/Services/RESTServer/src/main/java/aebd/group/webapp/EscreveFicheiro.java
+++ Ficha02/Services/RESTServer/src/main/java/aebd/group/webapp/EscreveFicheiro.java
@@ -3,11 +3,15 @@ package aebd.group.webapp;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.nio.file.Files;
 
 public class EscreveFicheiro {
 
     public static void escrever(String json, String id_pedido){
         try {
+            File dir = new File("..\\Received");
+            if(!dir.exists())
+                Files.createDirectory(dir.toPath());
             File fich = new File("..\\Received\\" + id_pedido + ".json");
             BufferedWriter buffWrite = new BufferedWriter(new FileWriter(fich.getPath()));
             buffWrite.write(json);
